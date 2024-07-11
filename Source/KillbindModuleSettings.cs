1ebb1b3c305837cf7b5ab839d5f44e68e7a13ecb
baltdev <heptor42@gmail.com>
2024-07-11 16:10:02 -0500

    Fix formatting

--- Source/KillbindModuleSettings.cs
+++ Source/KillbindModuleSettings.cs
@@ -1,5 +1,9 @@
+using Celeste.Mod;
+
 namespace Celeste.Mod.Killbind;
 
 public class KillbindModuleSettings : EverestModuleSettings {
+    [DefaultButtonBinding(Buttons.Back, Keys.Tab)]
+    public ButtonBinding YourButtonBinding { get; set; }
 
 }
